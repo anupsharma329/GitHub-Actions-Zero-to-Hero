95e8d16ba1edc7db85210b7546b0a7bcf5804b40
Anup Sharma <68842014+anupsharma329@users.noreply.github.com>
2025-03-16 20:30:10 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,7 @@
-# app.py
-# This is a test commit
-def add(a, b):
-    return a + b
+# src/test_addition.py
+from addition import add
 
 def test_add():
     assert add(1, 2) == 3
-    assert add(1, -1) == 0
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
